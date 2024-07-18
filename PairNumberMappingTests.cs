82f8fe33f4639ee408d7b0e7043fd9ca1c215cca
navanee867 <navanee777@gmail.com>
2024-07-18 13:25:08 +0530

    Create PairNumberMappingTests.cs

---
+++ PairNumberMappingTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    internal class PairNumberMappingTests
+    {
+        public static void RunTests()
+        {
+            ValidatePairNumberMapping(Color.Red, Color.Brown, 9);
+            ValidatePairNumberMapping(Color.Violet, Color.SlateGray, 25);
+            Assert.Throws<ArgumentException>(() => ValidatePairNumberMapping(Color.AliceBlue, Color.AntiqueWhite, 10));
+        }
+
+        private static void ValidatePairNumberMapping(Color majorColor, Color minorColor, int expectedPairNumber)
+        {
+            ColorPair colorPair = new ColorPair(majorColor, minorColor);
+            int pairNumber = ColorMapHelper.GetPairNumberFromColor(colorPair);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", colorPair, pairNumber);
+            Debug.Assert(pairNumber == expectedPairNumber);
+        }
+    }
+}
